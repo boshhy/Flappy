a5170c8314fce39c8bca75d80e9469abb757d70f
boshhy <boshhy101@yahoo.com>
2022-12-12 00:58:26 -0800

    Added randomization between pipe pair gap height

--- PipePair.lua
+++ PipePair.lua
@@ -10,8 +10,6 @@
 
 PipePair = Class{}
 
--- size of the gap between pipes
-local GAP_HEIGHT = 90
 
 function PipePair:init(y)
     -- flag to hold whether this pair has been scored (jumped through)
@@ -23,10 +21,13 @@ function PipePair:init(y)
     -- y value is for the topmost pipe; gap is a vertical shift of the second lower pipe
     self.y = y
 
+    -- Randomize a gap for the pipes between 80 to 120 pixels
+    self.gap_height = math.random(80, 120)
+
     -- instantiate two pipes that belong to this pair
     self.pipes = {
         ['upper'] = Pipe('top', self.y),
-        ['lower'] = Pipe('bottom', self.y + PIPE_HEIGHT + GAP_HEIGHT)
+        ['lower'] = Pipe('bottom', self.y + PIPE_HEIGHT + self.gap_height)
     }
 
     -- whether this pipe pair is ready to be removed from the scene
