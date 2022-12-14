37c0f52e6efb16c00ba44e78a341da14c87a90c9
boshhy <boshhy101@yahoo.com>
2022-12-13 23:29:00 -0800

    Added medals on the score state scene

--- states/ScoreState.lua
+++ states/ScoreState.lua
@@ -10,12 +10,17 @@
 
 ScoreState = Class{__includes = BaseState}
 
+local medal_one = love.graphics.newImage('assets/sprites/medal_one.png')
+local medal_two = love.graphics.newImage('assets/sprites/medal_two.png')
+local medal_three = love.graphics.newImage('assets/sprites/medal_three.png')
+
 --[[
     When we enter the score state, we expect to receive the score
     from the play state so we know what to render to the State.
 ]]
 function ScoreState:enter(params)
     self.score = params.score
+    self.image = medal_three
 end
 
 function ScoreState:update(dt)
@@ -23,6 +28,15 @@ function ScoreState:update(dt)
     if love.keyboard.wasPressed('enter') or love.keyboard.wasPressed('return') then
         gStateMachine:change('countdown')
     end
+
+    if self.score >= 4 then
+        self.image = medal_one
+    elseif self.score >= 2 then
+        self.image = medal_two
+    elseif self.score >= 0 then
+        self.image = medal_three
+    end
+
 end
 
 function ScoreState:render()
@@ -33,5 +47,7 @@ function ScoreState:render()
     love.graphics.setFont(mediumFont)
     love.graphics.printf('Score: ' .. tostring(self.score), 0, 100, VIRTUAL_WIDTH, 'center')
 
+    love.graphics.draw(self.image, VIRTUAL_WIDTH/2 - (self.image:getWidth() * 0.12)/2, 118, 0, 0.12, 0.12)
+    
     love.graphics.printf('Press Enter to Play Again!', 0, 160, VIRTUAL_WIDTH, 'center')
 end
